dda3adfa65f3e555207aef1a532ae38aa7263aee
BangJeongHyeok <hyeok020514@naver.com>
2025-11-08 15:47:54 +0900

    firestore도 붙이기

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx'
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
